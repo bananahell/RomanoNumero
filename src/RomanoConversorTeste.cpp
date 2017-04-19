6e01be410136a1e1522c5cc7c80f4f701ed953b1
BJ <cmcgb58912306345715@gmail.com>
2017-04-19 20:27:27 -0300

    Testes básicos - 5 testes

--- src/RomanoConversorTeste.cpp
+++ src/RomanoConversorTeste.cpp
@@ -13,11 +13,56 @@
 #include <RomanoNumero.hpp>
 
 
-TEST(InstanciandoRomanoNumero, InstanciandoNumeroRomano) {
+/**
+ * @brief Teste InstanciandoRomanoNumero, testando se a classe está sendo instanciada sem parâmetro.
+ *
+ */
+TEST(Instanciando, NenhumParametro) {
+  RomanoNumero* numTeste = new RomanoNumero();
+  EXPECT_EQ(0, numTeste->getNumeroDecimal());
+  delete numTeste;
+}
+/**
+ * @brief Teste InstanciandoRomanoNumero, testando se a classe está sendo instanciada com número decimal.
+ *
+ */
+TEST(Instanciando, NumeroDecimal) {
+  RomanoNumero* numTeste = new RomanoNumero(42);
+  EXPECT_EQ(42, numTeste->getNumeroDecimal());
+  delete numTeste;
+}
+/**
+ * @brief Teste InstanciandoRomanoNumero, testando se a classe está sendo instanciada com número romano.
+ *
+ */
+TEST(Instanciando, NumeroRomano) {
   RomanoNumero* numTeste = new RomanoNumero("XCVII");
   EXPECT_EQ("XCVII", numTeste->getNumeroRomano());
   delete numTeste;
 }
+
+/**
+ * @brief Teste InstanciandoRomanoNumero, testando se a classe está sendo instanciada com número romano.
+ *
+ */
+TEST(Setando, Decimal) {
+  RomanoNumero* numTeste = new RomanoNumero(42);
+  numTeste->setNumeroDecimal(97);
+  EXPECT_EQ(97, numTeste->getNumeroDecimal());
+  delete numTeste;
+}
+/**
+ * @brief Teste InstanciandoRomanoNumero, testando se a classe está sendo instanciada com número romano.
+ *
+ */
+TEST(Setando, Romano) {
+  RomanoNumero* numTeste = new RomanoNumero("XCVII");
+  numTeste->setNumeroRomano("XLII");
+  EXPECT_EQ("XLII", numTeste->getNumeroRomano());
+  delete numTeste;
+}
+
+
 /**
  * @brief Main do programa.
  *
